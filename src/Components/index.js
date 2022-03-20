1e4981980c8259e61ed7884677035898520d1c60
Skyleen77 <elsk8@outlook.fr>
2022-03-20 18:32:08 +0100

    setup src folder

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/prefer-default-export */
-export { default as Loader } from './Loader';
-export { default as Accordion } from './Accordion';
-export { default as Alert } from './Alert';
-export { default as Dropdown } from './Dropdown';
-export { default as Modal } from './Modal';
-export { default as Navbar } from './Navbar';
-export { default as Sidebar } from './Sidebar';
-export { default as Tab } from './Tab';
-export { default as Tag } from './Tag';
-export { default as Toast } from './Toast';
-export { default as ToTop } from './ToTop';
+export { default as Loader } from './Loader/Loader';
+export { default as Accordion } from './Accordion/Accordion';
+export { default as Alert } from './Alert/Alert';
+export { default as Dropdown } from './Dropdown/Dropdown';
+export { default as Modal } from './Modal/Modal';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Sidebar } from './Sidebar/Sidebar';
+export { default as Tab } from './Tab/Tab';
+export { default as Tag } from './Tag/Tag';
+export { default as Toast } from './Toast/Toast';
+export { default as ToTop } from './ToTop/ToTop';
