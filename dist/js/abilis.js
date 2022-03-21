412c0d67ba4554e0ca718fa8bdbbea1521649139
Skyleen77 <elsk8@outlook.fr>
2022-03-21 21:58:46 +0100

    fix navbar

--- dist/js/abilis.js
+++ dist/js/abilis.js
@@ -305,46 +305,43 @@
     }
 
     _createClass(Navbar, [{
-      key: "navDropClick",
-      value: function navDropClick(statut, navdrop, dropdown, linked) {
-        var link = navdrop.querySelector(linked);
-        var drop = navdrop.querySelector(dropdown);
-
-        if (drop && link) {
-          if (statut === 'close') {
-            if (drop.classList.contains('open')) {
-              drop.classList.remove('open');
-              link.classList.remove('open');
-            }
-          } else {
-            drop.classList.toggle('open');
-            link.classList.toggle('open');
-          }
-        }
-      }
-    }, {
       key: "navbarToggle",
       value: function navbarToggle(navToggle) {
-        var _this = this;
+        var navDropClick = function navDropClick(statut, navdrop, dropdown, linked) {
+          var link = navdrop.querySelector(linked);
+          var drop = navdrop.querySelector(dropdown);
+
+          if (drop && link) {
+            if (statut == 'close') {
+              if (drop.classList.contains("open")) {
+                drop.classList.remove('open');
+                link.classList.remove('open');
+              }
+            } else {
+              drop.classList.toggle('open');
+              link.classList.toggle('open');
+            }
+          }
+        };
 
         navToggle.forEach(function (nav) {
           // MOBILE
           var toggleMenu = document.querySelector(nav.dataset.toggleMenu);
           var navDropdown = document.querySelectorAll('.navbar-menu .navbar-item.dropdown');
           nav.addEventListener('click', function () {
-            if (nav.classList.contains('open')) {
-              nav.classList.remove('open');
-              toggleMenu.classList.remove('open');
+            if (nav.classList.contains("open")) {
+              nav.classList.remove("open");
+              toggleMenu.classList.remove("open");
               navDropdown.forEach(function (navDrop) {
-                if (navDrop.classList.contains('click')) {
-                  _this.navDropClick('close', navDrop, '.navbar-dropdown-clicked', '.navbar-link');
+                if (navDrop.classList.contains("click")) {
+                  navDropClick('close', navDrop, '.navbar-dropdown-clicked', '.navbar-link');
                 } else {
-                  _this.navDropClick('close', navDrop, '.navbar-dropdown', '.navbar-link');
+                  navDropClick('close', navDrop, '.navbar-dropdown', '.navbar-link');
                 }
               });
             } else {
-              nav.classList.add('open');
-              toggleMenu.classList.add('open');
+              nav.classList.add("open");
+              toggleMenu.classList.add("open");
             }
           });
           var btnClose = toggleMenu.querySelector('.close');
@@ -353,8 +350,8 @@
             btnClose.addEventListener('click', function () {
               toggleMenu.classList.toggle('open');
 
-              if (nav.classList.contains('open')) {
-                nav.classList.remove('open');
+              if (nav.classList.contains("open")) {
+                nav.classList.remove("open");
               }
             });
           } // MOBILE
@@ -363,10 +360,10 @@
 
           navDropdown.forEach(function (navDrop) {
             navDrop.querySelector('.navbar-link').addEventListener('click', function () {
-              if (navDrop.classList.contains('click')) {
-                _this.navDropClick('open', navDrop, '.navbar-dropdown-clicked', '.navbar-link');
+              if (navDrop.classList.contains("click")) {
+                navDropClick('open', navDrop, '.navbar-dropdown-clicked', '.navbar-link');
               } else {
-                _this.navDropClick('open', navDrop, '.navbar-dropdown', '.navbar-link');
+                navDropClick('open', navDrop, '.navbar-dropdown', '.navbar-link');
               }
             });
           }); // DROPDOWN
@@ -375,127 +372,125 @@
     }, {
       key: "navbarSticky",
       value: function navbarSticky(navbar) {
-        if (navbar) {
-          var datasClassBefore = navbar.querySelectorAll('[data-sticky-class-before]');
-          var datasClassAfter = navbar.querySelectorAll('[data-sticky-class-after]');
+        var datasClassBefore = navbar.querySelectorAll('[data-sticky-class-before]');
+        var datasClassAfter = navbar.querySelectorAll('[data-sticky-class-after]');
 
-          var stickyClassAction = function stickyClassAction(item, data, type) {
-            var classToAdd = data.split(' ');
-            classToAdd.forEach(function (e) {
-              type === 'add' ? item.classList.add(e) : item.classList.remove(e);
+        var stickyClassAction = function stickyClassAction(item, data, type) {
+          var classToAdd = data.split(' ');
+          classToAdd.forEach(function (e) {
+            type == 'add' ? item.classList.add(e) : item.classList.remove(e);
+          });
+        };
+
+        var stickyClassActionCondition = function stickyClassActionCondition(classType, action) {
+          (classType == 'before' ? navbar.dataset.stickyClassBefore : navbar.dataset.stickyClassAfter) ? stickyClassAction(navbar, classType == 'before' ? navbar.dataset.stickyClassBefore : navbar.dataset.stickyClassAfter, action) : '';
+          datasClassBefore.forEach(function (e) {
+            return stickyClassAction(e, classType == 'before' ? e.dataset.stickyClassBefore : e.dataset.stickyClassAfter, action);
+          });
+        };
+
+        var classToStickyAction = function classToStickyAction(type) {
+          if (type == 'sticky') {
+            stickyClassActionCondition('before', 'remove');
+            stickyClassActionCondition('after', 'add');
+          } else {
+            stickyClassActionCondition('before', 'add');
+            stickyClassActionCondition('after', 'remove');
+          }
+        };
+
+        stickyClassActionCondition('before', 'add'); // TYPE 1
+
+        var stickyType = navbar.dataset.stickyType;
+
+        if (stickyType) {
+          navbar.classList.add(stickyType);
+
+          var stickyClassToggle = function stickyClassToggle(e, type) {
+            var el = type == 'before' ? e.dataset.stickyClassBefore : e.dataset.stickyClassAfter;
+            el.split(' ').forEach(function (val) {
+              e.classList.toggle(val, type == 'before' ? window.scrollY <= 0 : window.scrollY > 0);
             });
           };
 
-          var stickyClassActionCondition = function stickyClassActionCondition(classType, action) {
-            (classType === 'before' ? navbar.dataset.stickyClassBefore : navbar.dataset.stickyClassAfter) ? stickyClassAction(navbar, classType === 'before' ? navbar.dataset.stickyClassBefore : navbar.dataset.stickyClassAfter, action) : '';
+          var stickyPerso = function stickyPerso() {
+            navbar.classList.toggle("sticky-active", window.scrollY > 0);
+            navbar.dataset.stickyClassBefore ? stickyClassToggle(navbar, 'before') : '';
             datasClassBefore.forEach(function (e) {
-              return stickyClassAction(e, classType === 'before' ? e.dataset.stickyClassBefore : e.dataset.stickyClassAfter, action);
+              return stickyClassToggle(e, 'before');
+            });
+            navbar.dataset.stickyClassAfter ? stickyClassToggle(navbar, 'after') : '';
+            datasClassAfter.forEach(function (e) {
+              return stickyClassToggle(e, 'after');
             });
           };
 
-          var classToStickyAction = function classToStickyAction(type) {
-            if (type === 'sticky') {
-              stickyClassActionCondition('before', 'remove');
-              stickyClassActionCondition('after', 'add');
-            } else {
-              stickyClassActionCondition('before', 'add');
-              stickyClassActionCondition('after', 'remove');
-            }
-          };
-
-          stickyClassActionCondition('before', 'add'); // TYPE 1
-
-          var stickyType = navbar.dataset.stickyType;
-
-          if (stickyType) {
-            navbar.classList.add(stickyType);
-
-            var stickyClassToggle = function stickyClassToggle(e, type) {
-              var el = type === 'before' ? e.dataset.stickyClassBefore : e.dataset.stickyClassAfter;
-              el.split(' ').forEach(function (val) {
-                e.classList.toggle(val, type === 'before' ? window.scrollY <= 0 : window.scrollY > 0);
-              });
-            };
-
-            var stickyPerso = function stickyPerso() {
-              navbar.classList.toggle('sticky-active', window.scrollY > 0);
-              navbar.dataset.stickyClassBefore ? stickyClassToggle(navbar, 'before') : '';
-              datasClassBefore.forEach(function (e) {
-                return stickyClassToggle(e, 'before');
-              });
-              navbar.dataset.stickyClassAfter ? stickyClassToggle(navbar, 'after') : '';
-              datasClassAfter.forEach(function (e) {
-                return stickyClassToggle(e, 'after');
-              });
-            };
-
-            if (navbar.dataset.path) {
-              var path = navbar.dataset.path.split(' ');
-              var url = new URL(document.location.href);
-              var isUrlValid = false;
-              path.forEach(function (p) {
-                if (url.pathname === "/".concat(p)) {
-                  isUrlValid = true;
-                  window.addEventListener('scroll', function () {
-                    stickyPerso();
-                  });
-                }
-              });
-
-              if (!isUrlValid) {
-                navbar.classList.add('sticky-active');
-                classToStickyAction('sticky');
+          if (navbar.dataset.path) {
+            var path = navbar.dataset.path.split(' ');
+            var url = new URL(document.location.href);
+            var isUrlValid = false;
+            path.forEach(function (p) {
+              if (url.pathname == "/".concat(p)) {
+                isUrlValid = true;
+                window.addEventListener('scroll', function () {
+                  stickyPerso();
+                });
               }
-            } else {
-              window.addEventListener('scroll', function () {
-                stickyPerso();
-              });
+            });
+
+            if (!isUrlValid) {
+              navbar.classList.add("sticky-active");
+              classToStickyAction('sticky');
             }
-          } // TYPE 1
-          // TYPE 2
-
-
-          if (navbar.dataset.stickyTop) {
-            var isSticky = false;
+          } else {
             window.addEventListener('scroll', function () {
-              if (window.scrollY > navbar.dataset.stickyTop && isSticky === false) {
-                navbar.classList.add('sticky');
-
-                if (navbar.dataset.stickyAnimation) {
-                  navbar.classList.add(navbar.dataset.stickyAnimation);
-                  setTimeout(function () {
-                    navbar.classList.remove(navbar.dataset.stickyAnimation);
-                  }, 500);
-                }
-
-                classToStickyAction('sticky');
-                isSticky = true;
-              }
-
-              if (window.scrollY < navbar.dataset.stickyTop && isSticky === true) {
-                if (navbar.dataset.stickyAnimation) {
-                  navbar.classList.add(navbar.dataset.stickyAnimation);
-                  setTimeout(function () {
-                    navbar.classList.remove(navbar.dataset.stickyAnimation);
-                  }, 500);
-                }
-
-                navbar.classList.remove('sticky');
-                classToStickyAction('top');
-                isSticky = false;
-              }
+              stickyPerso();
             });
-          } // TYPE 2
+          }
+        } // TYPE 1
+        // TYPE 2
 
-        }
+
+        if (navbar.dataset.stickyTop) {
+          var isSticky = false;
+          window.addEventListener('scroll', function () {
+            if (window.scrollY > navbar.dataset.stickyTop && isSticky == false) {
+              navbar.classList.add("sticky");
+
+              if (navbar.dataset.stickyAnimation) {
+                navbar.classList.add(navbar.dataset.stickyAnimation);
+                setTimeout(function () {
+                  navbar.classList.remove(navbar.dataset.stickyAnimation);
+                }, 500);
+              }
+
+              classToStickyAction('sticky');
+              isSticky = true;
+            }
+
+            if (window.scrollY < navbar.dataset.stickyTop && isSticky == true) {
+              if (navbar.dataset.stickyAnimation) {
+                navbar.classList.add(navbar.dataset.stickyAnimation);
+                setTimeout(function () {
+                  navbar.classList.remove(navbar.dataset.stickyAnimation);
+                }, 500);
+              }
+
+              navbar.classList.remove("sticky");
+              classToStickyAction('top');
+              isSticky = false;
+            }
+          });
+        } // TYPE 2
+
       }
     }, {
       key: "render",
       value: function render() {
-        var _this2 = this;
+        var _this = this;
 
         this.navToggles.forEach(function (navToggle) {
-          _this2.navbarToggle(document.querySelectorAll(navToggle));
+          _this.navbarToggle(document.querySelectorAll(navToggle));
         });
         this.navbarSticky(this.nav);
       }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,250 +7,198 @@ class Navbar {
       '.navbar-toggle-1',
       '.navbar-toggle-2',
       '.navbar-toggle-3',
-      '.navbar-toggle-4',
+      '.navbar-toggle-4'
     ];
 
     this.run();
   }
 
-  navDropClick(statut, navdrop, dropdown, linked) {
-    const link = navdrop.querySelector(linked);
-    const drop = navdrop.querySelector(dropdown);
-
-    if (drop && link) {
-      if (statut === 'close') {
-        if (drop.classList.contains('open')) {
-          drop.classList.remove('open');
-          link.classList.remove('open');
+  navbarToggle(navToggle) {
+    const navDropClick = (statut, navdrop, dropdown, linked) => {
+      const link = navdrop.querySelector(linked);
+      const drop = navdrop.querySelector(dropdown);
+  
+      if(drop && link)  {
+        if(statut == 'close') {
+          if (drop.classList.contains("open")) {
+            drop.classList.remove('open');
+            link.classList.remove('open');
+          }
+        } else {
+          drop.classList.toggle('open');
+          link.classList.toggle('open');
         }
-      } else {
-        drop.classList.toggle('open');
-        link.classList.toggle('open');
       }
     }
-  }
-
-  navbarToggle(navToggle) {
-    navToggle.forEach((nav) => {
-      // MOBILE
-      const toggleMenu = document.querySelector(nav.dataset.toggleMenu);
-      const navDropdown = document.querySelectorAll(
-        '.navbar-menu .navbar-item.dropdown'
-      );
-
-      nav.addEventListener('click', () => {
-        if (nav.classList.contains('open')) {
-          nav.classList.remove('open');
-          toggleMenu.classList.remove('open');
-          navDropdown.forEach((navDrop) => {
-            if (navDrop.classList.contains('click')) {
-              this.navDropClick(
-                'close',
-                navDrop,
-                '.navbar-dropdown-clicked',
-                '.navbar-link'
-              );
+  
+    navToggle.forEach(nav => {
+        // MOBILE
+        const toggleMenu = document.querySelector(nav.dataset.toggleMenu);
+        const navDropdown = document.querySelectorAll('.navbar-menu .navbar-item.dropdown');
+  
+        nav.addEventListener('click', () => {
+            if (nav.classList.contains("open")) {
+                nav.classList.remove("open");
+                toggleMenu.classList.remove("open");
+                navDropdown.forEach(navDrop => {
+                    if(navDrop.classList.contains("click")) {
+                        navDropClick('close', navDrop, '.navbar-dropdown-clicked', '.navbar-link');
+                    } else {
+                        navDropClick('close', navDrop, '.navbar-dropdown', '.navbar-link');
+                    }
+                });
             } else {
-              this.navDropClick(
-                'close',
-                navDrop,
-                '.navbar-dropdown',
-                '.navbar-link'
-              );
+                nav.classList.add("open");
+                toggleMenu.classList.add("open");
             }
-          });
-        } else {
-          nav.classList.add('open');
-          toggleMenu.classList.add('open');
+        });
+  
+        const btnClose = toggleMenu.querySelector('.close');
+        if(btnClose) {
+            btnClose.addEventListener('click', () => {
+                toggleMenu.classList.toggle('open');
+                if (nav.classList.contains("open")) {
+                    nav.classList.remove("open");
+                }
+            });
         }
-      });
-
-      const btnClose = toggleMenu.querySelector('.close');
-      if (btnClose) {
-        btnClose.addEventListener('click', () => {
-          toggleMenu.classList.toggle('open');
-          if (nav.classList.contains('open')) {
-            nav.classList.remove('open');
-          }
+        // MOBILE
+  
+        // DROPDOWN
+        navDropdown.forEach(navDrop => {
+            navDrop.querySelector('.navbar-link').addEventListener('click', () => {
+                if(navDrop.classList.contains("click")) {
+                    navDropClick('open', navDrop, '.navbar-dropdown-clicked', '.navbar-link');
+                } else {
+                    navDropClick('open', navDrop, '.navbar-dropdown', '.navbar-link');
+                }
+            });
         });
-      }
-      // MOBILE
-
-      // DROPDOWN
-      navDropdown.forEach((navDrop) => {
-        navDrop.querySelector('.navbar-link').addEventListener('click', () => {
-          if (navDrop.classList.contains('click')) {
-            this.navDropClick(
-              'open',
-              navDrop,
-              '.navbar-dropdown-clicked',
-              '.navbar-link'
-            );
-          } else {
-            this.navDropClick(
-              'open',
-              navDrop,
-              '.navbar-dropdown',
-              '.navbar-link'
-            );
-          }
-        });
-      });
-      // DROPDOWN
+        // DROPDOWN
+  
     });
   }
 
   navbarSticky(navbar) {
-    if (navbar) {
-      const datasClassBefore = navbar.querySelectorAll(
-        '[data-sticky-class-before]'
-      );
-      const datasClassAfter = navbar.querySelectorAll(
-        '[data-sticky-class-after]'
-      );
+    const datasClassBefore = navbar.querySelectorAll('[data-sticky-class-before]');
+    const datasClassAfter = navbar.querySelectorAll('[data-sticky-class-after]');
 
-      const stickyClassAction = (item, data, type) => {
+    const stickyClassAction = (item, data, type) => {
         const classToAdd = data.split(' ');
-        classToAdd.forEach((e) => {
-          type === 'add' ? item.classList.add(e) : item.classList.remove(e);
+        classToAdd.forEach(e => {
+            type == 'add' ? item.classList.add(e) : item.classList.remove(e);
         });
-      };
+    }
 
-      const stickyClassActionCondition = (classType, action) => {
-        (
-          classType === 'before'
-            ? navbar.dataset.stickyClassBefore
-            : navbar.dataset.stickyClassAfter
-        )
-          ? stickyClassAction(
-              navbar,
-              classType === 'before'
-                ? navbar.dataset.stickyClassBefore
-                : navbar.dataset.stickyClassAfter,
-              action
-            )
-          : '';
-        datasClassBefore.forEach((e) =>
-          stickyClassAction(
-            e,
-            classType === 'before'
-              ? e.dataset.stickyClassBefore
-              : e.dataset.stickyClassAfter,
-            action
-          )
-        );
-      };
+    const stickyClassActionCondition = (classType, action) => {
+        (classType == 'before' ? navbar.dataset.stickyClassBefore : navbar.dataset.stickyClassAfter) ? stickyClassAction(navbar, (classType == 'before' ? navbar.dataset.stickyClassBefore : navbar.dataset.stickyClassAfter), action) : '';
+        datasClassBefore.forEach(e => stickyClassAction(e, (classType == 'before' ? e.dataset.stickyClassBefore : e.dataset.stickyClassAfter), action));
+    }
 
-      const classToStickyAction = (type) => {
-        if (type === 'sticky') {
-          stickyClassActionCondition('before', 'remove');
-          stickyClassActionCondition('after', 'add');
+    const classToStickyAction = (type) => {
+        if(type == 'sticky') {
+            stickyClassActionCondition('before', 'remove');
+            stickyClassActionCondition('after', 'add');
         } else {
-          stickyClassActionCondition('before', 'add');
-          stickyClassActionCondition('after', 'remove');
+            stickyClassActionCondition('before', 'add');
+            stickyClassActionCondition('after', 'remove');
         }
-      };
+    }
 
-      stickyClassActionCondition('before', 'add');
+    stickyClassActionCondition('before', 'add');
 
-      // TYPE 1
-      const { stickyType } = navbar.dataset;
+    // TYPE 1
+    const stickyType = navbar.dataset.stickyType;
 
-      if (stickyType) {
+    if(stickyType) {
         navbar.classList.add(stickyType);
-
+        
         const stickyClassToggle = (e, type) => {
-          const el =
-            type === 'before'
-              ? e.dataset.stickyClassBefore
-              : e.dataset.stickyClassAfter;
-          el.split(' ').forEach((val) => {
-            e.classList.toggle(
-              val,
-              type === 'before' ? window.scrollY <= 0 : window.scrollY > 0
-            );
-          });
-        };
+            const el = type == 'before' ? e.dataset.stickyClassBefore : e.dataset.stickyClassAfter;
+            el.split(' ').forEach(val => {
+                e.classList.toggle(val, (type == 'before' ? window.scrollY <= 0 : window.scrollY > 0));
+            });
+        }
 
         const stickyPerso = () => {
-          navbar.classList.toggle('sticky-active', window.scrollY > 0);
+            navbar.classList.toggle("sticky-active", window.scrollY > 0);
 
-          navbar.dataset.stickyClassBefore
-            ? stickyClassToggle(navbar, 'before')
-            : '';
-          datasClassBefore.forEach((e) => stickyClassToggle(e, 'before'));
-          navbar.dataset.stickyClassAfter
-            ? stickyClassToggle(navbar, 'after')
-            : '';
-          datasClassAfter.forEach((e) => stickyClassToggle(e, 'after'));
-        };
-
-        if (navbar.dataset.path) {
-          const path = navbar.dataset.path.split(' ');
-          const url = new URL(document.location.href);
-          let isUrlValid = false;
-
-          path.forEach((p) => {
-            if (url.pathname === `/${p}`) {
-              isUrlValid = true;
-              window.addEventListener('scroll', () => {
-                stickyPerso();
-              });
-            }
-          });
-
-          if (!isUrlValid) {
-            navbar.classList.add('sticky-active');
-            classToStickyAction('sticky');
-          }
-        } else {
-          window.addEventListener('scroll', () => {
-            stickyPerso();
-          });
+            navbar.dataset.stickyClassBefore ? stickyClassToggle(navbar, 'before') : '';
+            datasClassBefore.forEach(e => stickyClassToggle(e, 'before'));
+            navbar.dataset.stickyClassAfter ? stickyClassToggle(navbar, 'after') : '';
+            datasClassAfter.forEach(e => stickyClassToggle(e, 'after'));
         }
-      }
-      // TYPE 1
 
-      // TYPE 2
-      if (navbar.dataset.stickyTop) {
+        if(navbar.dataset.path) {
+            const path = navbar.dataset.path.split(' ');
+            const url = new URL(document.location.href);
+            let isUrlValid = false;
+
+            path.forEach(p => {
+                if(url.pathname == `/${p}`) {
+                    isUrlValid = true;
+                    window.addEventListener('scroll', () => {
+                        stickyPerso();
+                    });
+                }
+            });
+
+            if(!isUrlValid) {
+                navbar.classList.add("sticky-active");
+                classToStickyAction('sticky');
+            }
+        } else {
+            window.addEventListener('scroll', () => {
+                stickyPerso();
+            });
+        }
+    }
+    // TYPE 1
+    
+    // TYPE 2
+    if(navbar.dataset.stickyTop) {
+
         let isSticky = false;
 
         window.addEventListener('scroll', () => {
-          if (window.scrollY > navbar.dataset.stickyTop && isSticky === false) {
-            navbar.classList.add('sticky');
 
-            if (navbar.dataset.stickyAnimation) {
-              navbar.classList.add(navbar.dataset.stickyAnimation);
-              setTimeout(() => {
-                navbar.classList.remove(navbar.dataset.stickyAnimation);
-              }, 500);
+            if(window.scrollY > navbar.dataset.stickyTop && isSticky == false) {
+                navbar.classList.add("sticky");
+
+                if(navbar.dataset.stickyAnimation) {
+                    navbar.classList.add(navbar.dataset.stickyAnimation);
+                    setTimeout(() => {
+                        navbar.classList.remove(navbar.dataset.stickyAnimation);
+                    }, 500);
+                }
+                
+                classToStickyAction('sticky');
+                isSticky = true;
             }
 
-            classToStickyAction('sticky');
-            isSticky = true;
-          }
+            if(window.scrollY < navbar.dataset.stickyTop && isSticky == true) {
 
-          if (window.scrollY < navbar.dataset.stickyTop && isSticky === true) {
-            if (navbar.dataset.stickyAnimation) {
-              navbar.classList.add(navbar.dataset.stickyAnimation);
-              setTimeout(() => {
-                navbar.classList.remove(navbar.dataset.stickyAnimation);
-              }, 500);
+                if(navbar.dataset.stickyAnimation) {
+                    navbar.classList.add(navbar.dataset.stickyAnimation);
+                    setTimeout(() => {
+                        navbar.classList.remove(navbar.dataset.stickyAnimation);
+                    }, 500);
+                }
+
+                navbar.classList.remove("sticky");
+                classToStickyAction('top');
+
+                isSticky = false;
             }
 
-            navbar.classList.remove('sticky');
-            classToStickyAction('top');
-
-            isSticky = false;
-          }
         });
-      }
-      // TYPE 2
+
     }
+    // TYPE 2
   }
 
   render() {
-    this.navToggles.forEach((navToggle) => {
+    this.navToggles.forEach(navToggle => {
       this.navbarToggle(document.querySelectorAll(navToggle));
     });
 
